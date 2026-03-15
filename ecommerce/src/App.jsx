718a9d657384bae36ce48d1e038481c61cd417d4
Katlego Twala <twalakatlego44@gmail.com>
2026-03-15 13:39:20 +0200

    Defined all the neccessary routes, imported every page component and mapped it to a URL path

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,1 +1,25 @@
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import ProductDetail from './pages/ProductDetail';
+import CartPage from './pages/CartPage';
+import CheckoutPage from './pages/CheckoutPage';
+import AddAddress from './pages/AddAddress';
+import AddPayment from './pages/AddPayment';
+import OrderSuccess from './pages/OrderSuccess';
 
+function App() {
+    return (
+        //Routes picks the first <Router> whose path matches the current URL
+         <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/product/:id' element={<ProductDetail />} />
+            <Route path='/cart' element={<CartPage />} />
+            <Route path='/checkout' element={<CheckoutPage />} />
+            <Route path='/add-address' element={<AddAddress />} />
+            <Route path='/add-payment' element={<AddPayment />} />
+            <Route path='/order-success' element={<OrderSuccess />} />
+        </Routes>
+    );
+}
+
+export default App;
