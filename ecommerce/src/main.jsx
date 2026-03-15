5513176b9b80aa8d5925c6da480b2bfe8808c992
Katlego Twala <twalakatlego44@gmail.com>
2026-03-15 13:28:25 +0200

    adjusted main.jsx enabling routing and Redux to work in the app

--- ecommerce/src/main.jsx
+++ ecommerce/src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store/store';
 import './index.css'
 import App from './App.jsx'
 
+//Find the <div id='root'> in index.html and render the App component inside it
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/*Provider makes the Redux store available to any nested components that need to access the Redux store.*/}
+    <Provider store={store}>
+      <BrowserRouter>
+      {/*BrowserRouter enables URL-based navigation */}
+        <App />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
-)
+);
